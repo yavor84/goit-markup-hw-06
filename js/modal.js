d1ef58a46bd387e4358be77d6bb2beb48683d96b
Yavor <pojardim@gmail.com>
2025-04-10 15:41:16 +0300

    fix bugs

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,34 @@
 (() => {
   const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
     openModalBtn: document.querySelector('[data-modal-open]'),
-    // Додати атрибут data-modal-close на кнопку закриття
     closeModalBtn: document.querySelector('[data-modal-close]'),
-    // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector('[data-modal]'),
+    body: document.body,
   };
+
+  function handleEscKeyPress(event) {
+    if (event.key === 'Escape' && refs.modal.classList.contains('is-open')) {
+      toggleModal();
+    }
+  }
+
+  function toggleModal() {
+    const isModalOpen = refs.modal.classList.toggle('is-open');
+    refs.body.classList.toggle('no-scroll');
+
+    if (isModalOpen) {
+      document.addEventListener('keydown', handleEscKeyPress);
+    } else {
+      document.removeEventListener('keydown', handleEscKeyPress);
+    }
+  }
+
+  refs.modal.addEventListener('click', event => {
+    if (event.target === refs.modal) {
+      toggleModal();
+    }
+  });
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle('is-open');
-  }
 })();
